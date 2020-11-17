d0d402c49c95e3c59d5158012d10c8c920d3d668
LucasMarquesdeSousa <marqueslukas66@gmail.com>
2020-11-17 19:23:29 -0300

    master

--- app.js
+++ app.js
@@ -3,23 +3,42 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const handlebars = require('express-handlebars');
 const app = express();
-const urlencodeParser = bodyParser.urlencoded({extended:false});
+const urlencodeParser = bodyParser.urlencoded({ extended: false });
 
 //templeats
-app.engine("handlebars", handlebars({defaultLayout: 'main'}));
+app.engine("handlebars", handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 //rotas
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.render('index');
 });
-app.get('/style', function(req, res){
-    res.sendFile(__dirname+'/css/style.css')
+app.get('/style', function(req, res) {
+    res.sendFile(__dirname + '/css/style.css')
 })
-app.get('/logo', function(req, res){
-    res.sendFile(__dirname+'/img/Logo.png')
+app.get('/logo', function(req, res) {
+    res.sendFile(__dirname + '/img/Logo.png')
 })
+app.get('/cad', function(req, res) {
+    res.render('CadUsuario');
+})
+app.get('/bolos', function(req, res) {
+    res.sendFile(__dirname + '/img/Bolo1.png')
+});
+app.get('/logouts', function(req, res) {
+    res.render('Entrar');
+})
+app.get('/cadped', function(req, res) {
+    res.render('CadPedido')
+})
+
+
 //Iniciar servidor
-app.listen(3000, function(req, res){
+app.listen(3000, function(req, res) {
     console.log("Servidor rodando!!!!!");
 });
+module.exports = (req, res) => {
+    const { name = 'World' } = req.query
+    res.status(200).send(`Hello ${name}!`)
+
+}
